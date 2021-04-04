dc9ecd62047cf58cb21af088c3f0bda079da0cf8
Alexey Kovalsky <alexey.kovalsky@utdweb.team>
2021-04-04 16:36:00 +0300

    init

---
+++ pages/contact-us.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        Contact
+      </h1>
+      <div class="links">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/contact-us">Contact us</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
